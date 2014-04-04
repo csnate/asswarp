0c8d3cd4839e20f4efe3c2fabc3d1cfc7ee91c06
Nate Tovo <ntovo@comscore.com>
2014-04-04 12:59:38 -0500

    clean up

--- controller/scoring.py
+++ controller/scoring.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import spidev
  
-GPIO.setmode(GPIO.BCM)
-DEBUG = 1
+#GPIO.setmode(GPIO.BCM)
+#DEBUG = 1
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -29,123 +29,43 @@ def get_adc(channel):
     ret = ((r[1]&31) << 6) + (r[2] >> 2)
     return ret
  
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        #if ((adcnum > 1) or (adcnum < 0)):
-        #        return 0
-        GPIO.output(cspin, True)
- 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
- 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
- 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
- 
-        GPIO.output(cspin, True)
-        
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
- 
+
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
-SPICLK = 18
-SPIMISO = 23
-SPIMOSI = 24
-SPICS = 25
+#SPICLK = 18
+#SPIMISO = 23
+#SPIMOSI = 24
+#SPICS = 25
  
 # set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
+#GPIO.setup(SPIMOSI, GPIO.OUT)
+#GPIO.setup(SPIMISO, GPIO.IN)
+#GPIO.setup(SPICLK, GPIO.OUT)
+#GPIO.setup(SPICS, GPIO.OUT)
  
 # Vibration Sensor #1 attached to #adc0, Sensor #2 attached to adc#1
-team_1 = 0;
-team_2 = 1;
+team_1 = 0
+team_2 = 1
  
-#last_read = 0       # this keeps track of the last potentiometer value
-team_1_tolerance = 30;           # to keep from being jittery we'll only change
-team_2_tolerance = 40;                    # volume when the pot has moved more than 5 'counts'
-
-team_1_score = 0;
-team_2_score = 0;
+# tolerance levels
+team_1_tolerance = 50
+team_2_tolerance = 50
 
 try:
             
      while True:
-     # we'll assume that the pot didn't move
-     #trim_pot_changed = False
-    
-     
+
          # read the analog pin
-         #team_1_read = readadc(team_1, SPICLK, SPIMOSI, SPIMISO, SPICS)
-         #team_2_read = readadc(team_2, SPICLK, SPIMOSI, SPIMISO, SPICS)
-         team_1_read = get_adc(team_1);
-         team_2_read = get_adc(team_2);
-         
-         # how much has it changed since the last read?
-         #pot_adjust = abs(trim_pot - last_read)
+         team_1_read = get_adc(team_1)
+         team_2_read = get_adc(team_2)
     
          #if DEBUG:
          if team_1_read > team_1_tolerance:
              print "[DEBUG] TEAM 1: ", team_1_read
-             team_1_score = team_1_score + 1
-             print "SCORE: Team 1: ", team_1_score, " :: Team 2: ", team_2_score
+             
          if team_2_read > team_2_tolerance:
              print "[DEBUG] TEAM 2: ", team_2_read
-             team_2_score = team_2_score + 1
-             print "SCORE: Team 1: ", team_1_score, " :: Team 2: ", team_2_score
-         if team_1_score > 4 or team_2_score > 4:
-             if team_1_score > 4:
-                 print "----------  TEAM 1 WINS! ---------"
-             elif team_2_score > 4:
-                 print "----------  TEAM 2 WINS! ---------"
-             team_1_score = 0
-             team_2_score = 0
     
-     #if ( team_1_read > tolerance ):
-     #       trim_pot_changed = True
-    
-     #if DEBUG:
-     #        print "trim_pot_changed", trim_pot_changed
-    
-     #if ( trim_pot_changed ):
-     #set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
-     #set_volume = round(set_volume)          # round out decimal value
-     #set_volume = int(set_volume)            # cast volume as integer
-    
-     #print 'Volume = {volume}%' .format(volume = set_volume)
-     #set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-     #os.system(set_vol_cmd)  # set volume
-    
-     #if DEBUG:
-     #       print "set_volume", set_volume
-     #       print "tri_pot_changed", set_volume
-    
-    # save the potentiometer reading for the next loop
-     #last_read = trim_pot
-    
-     # hang out and do nothing for a half second
          time.sleep(0.1)
 except:
-    print "Exception occured";
-finally:
-    GPIO.cleanup()
+    print "Exception occurred"
